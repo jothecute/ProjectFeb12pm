c3d4cfe00bd8bd9aa655aa847bd8ef504c41ff06
jothecute <jothecute4@gmail.com>
2022-02-06 22:08:13 +0530

    task done

---
+++ src/org/bny/Employee.java
@@ -0,0 +1,13 @@
+package org.bny;
+
+public class Employee {
+
+	private void Details() {
+		System.out.println("number");
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.Details();
+	}
+}
